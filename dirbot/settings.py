d0ed4267821888c2f93c45118e7c604ea9d2b68e
liudonghua <shaphone2009@gmail.com>
2019-08-01 05:18:29 -0700

    fix day 31 error

--- dirbot/settings.py
+++ dirbot/settings.py
@@ -8,8 +8,10 @@ SPIDER_MODULES = ['dirbot.spiders']
 NEWSPIDER_MODULE = 'dirbot.spiders'
 DEFAULT_ITEM_CLASS = 'dirbot.items.sitemap'
 ITEM_PIPELINES = {'dirbot.pipelines.savePipeline': 1}
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+DOWNLOAD_DELAY = 5
 
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+COOKIES_ENABLED = False
 
 def setup_django_env():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gwapp.settings")
--- map/views.py
+++ map/views.py
@@ -8,17 +8,20 @@ from django.http import JsonResponse
 
     
 def getRange(request):
-        now         = datetime.datetime.now()
+       
         range       = request.POST
-        
+        min_price=0 if not range['price'] else int(range['price'])/2
+        max_price=10000 if not range['price'] else int(range['price'])*2
         locations   = []
-        locations   = Bdmap.objects.filter(created__gte=datetime.datetime(now.year,now.month-1,now.day,tzinfo=None)
+        locations   = Bdmap.objects.filter(created__gte=(datetime.datetime.now()-datetime.timedelta(days=30))
                                     ).filter(lng__gt=range['west']
                                     ).filter(lng__lt=range['east']
                                     ).filter(lat__gt=range['south']
                                     ).filter(lat__lt=range['north']
+                                    ).filter(price__gt=min_price
+                                    ).filter(price__lt=max_price
                                     ).order_by('-created'
-                                    ).values('price','lng','lat','url','img','detail')[0:50]
+                                    ).values('price','lng','lat','url','img','detail')[0:100]
 
         if locations!=[]:
             #return JsonResponse(data=list(locations),safe=False)
